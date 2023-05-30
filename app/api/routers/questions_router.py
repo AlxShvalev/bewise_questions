29aa256f084e3dad462888721a2048c018e207b9
AlxShvalev <shvalevalx@gmail.com>
2023-05-30 13:26:29 +0500

    added description to questions router

--- app/api/routers/questions_router.py
+++ app/api/routers/questions_router.py
@@ -17,11 +17,15 @@ router = APIRouter(prefix="/questions", tags=["Questions"])
     status_code=HTTPStatus.OK,
     response_model=List[QuestionResponse],
     response_model_exclude_none=True,
-    summary="Get questions"
+    summary="Get new questions"
 )
 async def get_questions(
         request: QuestionsRequest,
         session: AsyncSession = Depends(get_async_session)
 ) -> List[QuestionResponse]:
+    """Get new quiz questions from remote server.
+
+    - **questions_num**: number of questions to be asked.
+    """
     result = await question_service.get_and_save_questions(request.questions_num, session)
     return result
